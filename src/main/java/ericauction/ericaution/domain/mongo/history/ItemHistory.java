4591eb24a842569cac47e4b3c53650620df749c9
qsc7342 <qsc7342@hanyang.ac.kr>
2022-10-04 19:42:01 +0900

    [ERICA-10] HistoryRepository 추가

--- src/main/java/ericauction/ericaution/domain/mongo/history/ItemHistory.java
+++ src/main/java/ericauction/ericaution/domain/mongo/history/ItemHistory.java
@@ -1,2 +1,9 @@
-package ericauction.ericaution.domain.mongo.history;public class ItemHistory {
+package ericauction.ericaution.domain.mongo.history;
+
+import ericauction.ericaution.domain.mongo.base.PersistableDocument;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class ItemHistory extends PersistableDocument {
+
 }
---
+++ src/main/java/ericauction/ericaution/interfaces/repository/mongo/history/ItemHistoryRepository.java
@@ -0,0 +1,2 @@
+package ericauction.ericaution.interfaces.repository.mongo.history;public class ItemHistoryRepository {
+}
